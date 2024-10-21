5ae7d8c2e79ec022481aee173d3cffc6a9f54d4e
Ye Xinjian <e0544100@u.nus.edu>
2024-10-21 15:09:07 +0800

    Add matching service to api gateway

--- backend/api-gateway/src/controllers/gateway.controller.ts
+++ backend/api-gateway/src/controllers/gateway.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, All, Req, Res } from '@nestjs/common';
+import { Controller, All, Req, Res, Get, Post } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Request, Response } from 'express';
 import { GatewayService } from '../services/gateway.service';
@@ -7,12 +7,15 @@ import { GatewayService } from '../services/gateway.service';
 export class GatewayController {
   private readonly questionServiceDomain: string;
   private readonly userServiceDomain: string;
+  private readonly matchingServiceDomain: string;
+
   constructor(
     private readonly gatewayService: GatewayService,
     private configService: ConfigService
   ){
     this.questionServiceDomain = this.configService.get<string>('QUESTION_SERVICE_DOMAIN');
     this.userServiceDomain = this.configService.get<string>('USER_SERVICE_DOMAIN');
+    this.matchingServiceDomain = this.configService.get<string>('MATCHING_SERVICE_DOMAIN');
   }
 
   @All('/')
@@ -20,15 +23,34 @@ export class GatewayController {
     res.status(200).json({ message: "Hello!" })
   }
 
-  // Need auth in the future (with secret key)
+  // Question service
   @All('question*')
   async handleQuestionRequest(@Req() req: Request, @Res() res: Response): Promise<void> {
     this.gatewayService.handleRedirectRequest(req, res, this.questionServiceDomain)
   }
 
-  // authenticated alrdy
+  // User service
   @All('users*')
   async handleUserRequest(@Req() req: Request, @Res() res: Response): Promise<void> {
     this.gatewayService.handleRedirectRequest(req, res, this.userServiceDomain)
   }
+  @All('auth*') // somehow cannot stack (or reuse)
+  async handleAuthRequest(@Req() req: Request, @Res() res: Response): Promise<void> {
+    this.gatewayService.handleRedirectRequest(req, res, this.userServiceDomain)
+  }
+
+  // Matching service (can't stack...)
+  @All('match*')
+  async handleMatchingRequest(@Req() req: Request, @Res() res: Response): Promise<void> {
+    this.gatewayService.handleRedirectRequest(req, res, this.matchingServiceDomain)
+  }
+  @Post('cancel-match*')
+  async handleCancelMatchRequest(@Req() req: Request, @Res() res: Response): Promise<void> {
+    this.gatewayService.handleRedirectRequest(req, res, this.matchingServiceDomain)
+  }
+  @Get('check-match*')
+  async handleCheckMatchRequest(@Req() req: Request, @Res() res: Response): Promise<void> {
+    this.gatewayService.handleRedirectRequest(req, res, this.matchingServiceDomain)
+  }
+
 }
