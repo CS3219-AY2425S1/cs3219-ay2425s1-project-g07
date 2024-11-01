372a1908d550d86b61a393e16147391b0902854b
Ye Xinjian <e0544100@u.nus.edu>
2024-11-01 13:52:42 +0800

    Add utility build script

---
+++ backend/history-service/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# utility script to build image on GKE
+version=$1
+
+if [[ "X$version" == "X" ]]; then
+    echo "Usage: bash build.sh [image_tag]"
+    echo "E.g. bash build.sh v1.1"
+    exit 1
+fi
+
+image="asia-southeast1-docker.pkg.dev/cs3219-peerprep-436507/peerprep/history-service:${version}"
+
+docker build -f Dockerfile.dev -t $image .
+
+docker push $image
